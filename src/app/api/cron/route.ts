54af81d697229918d643eb90621d120cdbd9a0e4
LuizGandra <luizc.gandra29@gmail.com>
2025-06-02 09:38:29 -0300

    fix: remove cron parameters

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,16 +1,6 @@
 import { updateCurrentDay } from '@/lib/redis'
 
 export async function GET(request: Request) {
-	const url = new URL(request.url)
-	const secret = url.searchParams.get('secret')
-	const expectedSecret = process.env.CRON_SECRET
-
-	console.log('secret: ', secret)
-	console.log('expectedSecret: ', expectedSecret)
-
-	if (secret !== expectedSecret)
-		return new Response('Unauthorized', { status: 401 })
-
 	try {
 		const newDay = await updateCurrentDay()
 
